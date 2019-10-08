2e2d17888f418762ae307a3f8e3d0fd3cf82391c
LuukNoverraz <luuk.noverraz@coderclass.nl>
2019-10-08 20:52:14 +0200

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float force = 2f;
+    // public bool isTarget = false;
+    public float zFactor = 2f;
+    public Vector3 startPosition;
+ 
+    private Vector2 startSwipe;
+    private Vector2 endSwipe;
+    void Start () {
+        startPosition = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
+    }
+    void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            startSwipe = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        }
+ 
+        if (Input.GetMouseButtonUp(0))
+        {
+            endSwipe = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+            // if (isTarget == true)
+            // {
+            Swipe();
+            // isTarget = false;
+            // }
+        }
+        if (transform.position.y < -5.0f) {
+            transform.position = startPosition;
+            rb.velocity = new Vector3(0.0f, 0.0f, 0.0f);
+        }
+    }
+ 
+    void Swipe()
+    {
+        Vector3 swipe = endSwipe - startSwipe;
+        swipe.z = swipe.y / zFactor;
+        swipe.y = 0.0f;
+        rb.AddForce(swipe * -force, ForceMode.Impulse);
+    }
+ 
+    private void OnMouseDown()
+    {
+        rb.constraints = RigidbodyConstraints.None;
+        // isTarget = true;
+    }
+}
---
+++ Assets/SwipeScript.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeScript : MonoBehaviour {
+
+	Vector2 startPos, endPos, direction; // touch start position, touch end position, swipe direction
+	float touchTimeStart, touchTimeFinish, timeInterval; // to calculate swipe time to sontrol throw force in Z direction
+
+	[SerializeField]
+	float throwForceInXandY = 1f; // to control throw force in X and Y directions
+
+	[SerializeField]
+	float throwForceInZ = 50f; // to control throw force in Z direction
+
+	Rigidbody rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		// if you touch the screen
+		if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Began) {
+
+			// getting touch position and marking time when you touch the screen
+			touchTimeStart = Time.time;
+			startPos = Input.GetTouch (0).position;
+		}
+
+		// if you release your finger
+		if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Ended) {
+
+			// marking time when you release it
+			touchTimeFinish = Time.time;
+
+			// calculate swipe time interval 
+			timeInterval = touchTimeFinish - touchTimeStart;
+
+			// getting release finger position
+			endPos = Input.GetTouch (0).position;
+
+			// calculating swipe direction in 2D space
+			direction = startPos - endPos;
+
+			// add force to balls rigidbody in 3D space depending on swipe time, direction and throw forces
+			rb.isKinematic = false;
+			rb.AddForce (- direction.x * throwForceInXandY, - direction.y * throwForceInXandY, throwForceInZ / timeInterval);
+
+			// Destroy ball in 4 seconds
+			Destroy (gameObject, 3f);
+
+		}
+			
+	}
+}
+
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class PlayerController : MonoBehaviour
+// {
+//     Vector3 startPos, endPos, direction;
+//     private float touchTimeStart, touchTimeFinish, timeInterval;
+//     public float throwForceInXandY = 1.0f;
+//     public float throwForceInZ = 50.0f;
+//     private Rigidbody rb;
+//     void Start()
+//     {
+//         rb = GetComponent<Rigidbody>();
+//     }
+
+//     void Update()
+//     {
+        
+//     }
+// }
