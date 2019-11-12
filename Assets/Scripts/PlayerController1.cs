2341589ca84ed46ed12370eeb9ed90e113813156
LuukNoverraz <luuk.noverraz@coderclass.nl>
2019-11-12 22:01:46 +0100

    [ADD] swiping on other screen fixed and power up test

--- Assets/Scripts/PlayerController1.cs
+++ Assets/Scripts/PlayerController1.cs
@@ -7,12 +7,10 @@ using UnityEngine.SceneManagement;
 public class PlayerController1 : MonoBehaviour
 {
     public Rigidbody rb;
-    public float force = 2f;
+    public float force;
     public Image player1RectImage;
     private float shootLaunch = 1f;
-    // public bool isTarget = false;
-    public float zFactor = 2f;
-    public Vector3 startPosition;
+    private bool player1Launching = false;
     private Vector2 startSwipe;
     private Vector2 endSwipe;
     public Text blueText;
@@ -21,22 +19,14 @@ public class PlayerController1 : MonoBehaviour
     public Text[] restartText;
     int blueScore = 0;
     public float[] colorList;
-
-    void Start()
-    {
-        startPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-    }
     void Update()
     {
         blueText.text = "SCORE: " + blueScore;
         if (blueScore >= 3)
         {
-            redWin.text = "BLUE WINS";
-            redWin.color = new Color(colorList[3], colorList[4], colorList[5], 1.0f);
-            restartText[0].color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
-            restartText[1].color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
-            blueWin.text = "BLUE WINS";
-            blueWin.color = new Color(colorList[3], colorList[4], colorList[5], 1.0f);
+            redWin.text = blueWin.text = "BLUE WINS";
+            redWin.color = blueWin.color = new Color(colorList[3], colorList[4], colorList[5], 1.0f);
+            restartText[0].color = restartText[1].color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
             Time.timeScale = 0;
             if (Input.GetMouseButtonDown(0))
             {
@@ -47,37 +37,42 @@ public class PlayerController1 : MonoBehaviour
         if (Input.GetMouseButtonDown(0) && Camera.main.ScreenToViewportPoint(Input.mousePosition).y <= 0.5)
         {
             startSwipe = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+            player1Launching = true;
         }
 
-        if (Input.GetMouseButtonUp(0) && Camera.main.ScreenToViewportPoint(Input.mousePosition).y <= 0.5)
+        if (Input.GetMouseButtonUp(0) && player1Launching)
         {
             endSwipe = Camera.main.ScreenToViewportPoint(Input.mousePosition);
-            // if (isTarget == true)
-            // {
             Swipe();
-            // isTarget = false;
-            // }
+            player1Launching = false;
         }
 
         if (transform.position.y < -5.0f)
         {
-            transform.position = startPosition;
-            rb.velocity = new Vector3(0.0f, 0.0f, 0.0f);
+            transform.position = new Vector3(0.0f, 2.0f, -2.0f);
+            rb.velocity = Vector3.zero;
             blueScore++;
         }
     }
- 
     void Swipe()
     {
         Vector3 swipe = endSwipe - startSwipe;
-        swipe.z = swipe.y / zFactor;
+        swipe.z = swipe.y;
         swipe.y = 0.0f;
         rb.AddForce(swipe * (force * shootLaunch), ForceMode.Impulse);
     }
- 
-    private void OnMouseDown()
+    void OnCollisionEnter(Collision collision)
     {
-        rb.constraints = RigidbodyConstraints.None;
-        // isTarget = true;
+        if (collision.gameObject.tag == "PowerUp")
+        {
+            Destroy(collision.gameObject);
+            transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+            StartCoroutine(PowerUpTimer());
+        }
+    }
+    IEnumerator PowerUpTimer() 
+    {
+        yield return new WaitForSeconds(3);
+        transform.localScale = new Vector3(0.75f, 0.75f, 0.75f);
     }
 }
--- Assets/Scripts/PlayerController2.cs
+++ Assets/Scripts/PlayerController2.cs
@@ -7,12 +7,10 @@ using UnityEngine.SceneManagement;
 public class PlayerController2 : MonoBehaviour
 {
     public Rigidbody rb;
-    public float force = 2f;
+    public float force;
     public Image player2RectImage;
     private float shootLaunch = 1f;
-    // public bool isTarget = false;
-    public float zFactor = 2f;
-    public Vector3 startPosition;
+    private bool player2Launching = false;
     private Vector2 startSwipe;
     private Vector2 endSwipe;
     public Text redText;
@@ -21,22 +19,14 @@ public class PlayerController2 : MonoBehaviour
     public Text[] restartText;
     int redScore = 0;
     public float[] colorList;
-
-    void Start()
-    {
-        startPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-    }
     void Update()
     {
         redText.text = "SCORE: " + redScore;
         if (redScore >= 3)
         {
-            redWin.text = "RED WINS";
-            redWin.color = new Color(colorList[0], colorList[1], colorList[2], 1.0f);
-            restartText[0].color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
-            restartText[1].color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
-            blueWin.text = "RED WINS";
-            blueWin.color = new Color(colorList[0], colorList[1], colorList[2], 1.0f);
+            redWin.text = blueWin.text = "RED WINS";
+            redWin.color = blueWin.color = new Color(colorList[0], colorList[1], colorList[2], 1.0f);
+            restartText[0].color = restartText[1].color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
             Time.timeScale = 0;
             if (Input.GetMouseButtonDown(0))
             {
@@ -47,37 +37,42 @@ public class PlayerController2 : MonoBehaviour
         if (Input.GetMouseButtonDown(0) && Camera.main.ScreenToViewportPoint(Input.mousePosition).y >= 0.5)
         {
             startSwipe = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+            player2Launching = true;
         }
 
-        if (Input.GetMouseButtonUp(0) && Camera.main.ScreenToViewportPoint(Input.mousePosition).y >= 0.5)
+        if (Input.GetMouseButtonUp(0) && player2Launching)
         {
             endSwipe = Camera.main.ScreenToViewportPoint(Input.mousePosition);
-            // if (isTarget == true)
-            // {
             Swipe();
-            // isTarget = false;
-            // }
+            player2Launching = false;
         }
 
         if (transform.position.y < -5.0f)
         {
-            transform.position = startPosition;
-            rb.velocity = new Vector3(0.0f, 0.0f, 0.0f);
+            transform.position = new Vector3(0.0f, 2.0f, 2.0f);
+            rb.velocity = Vector3.zero;
             redScore++;
         }
     }
-
     void Swipe()
     {
         Vector3 swipe = endSwipe - startSwipe;
-        swipe.z = swipe.y / zFactor;
+        swipe.z = swipe.y;
         swipe.y = 0.0f;
         rb.AddForce(swipe * (force * shootLaunch), ForceMode.Impulse);
     }
-
-    private void OnMouseDown()
+    void OnCollisionEnter(Collision collision)
     {
-        rb.constraints = RigidbodyConstraints.None;
-        // isTarget = true;
+        if (collision.gameObject.tag == "PowerUp")
+        {
+            Destroy(collision.gameObject);
+            transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+            StartCoroutine(PowerUpTimer());
+        }
+    }
+    IEnumerator PowerUpTimer() 
+    {
+        yield return new WaitForSeconds(3);
+        transform.localScale = new Vector3(0.75f, 0.75f, 0.75f);
     }
 }
