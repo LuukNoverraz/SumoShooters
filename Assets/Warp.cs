f9e0ae2856b7d928a126cb07e7bb60c9039dcf00
Luuk <luuknoverraz@gmail.com>
2020-01-25 14:04:16 +0100

    [ADD] Reworking maps

---
+++ Assets/Warp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warp : MonoBehaviour
+{
+    
+    [SerializeField] private Vector3 warpLocation;
+
+    void OnTriggerEnter(Collider collision)
+    {
+        // Check which warp is being touched
+        if (gameObject.layer == 25)
+        {
+            Debug.Log("go to 1");
+        }
+        if (gameObject.layer == 26)
+        {
+            Debug.Log("go to 2");
+        }
+    }
+}
